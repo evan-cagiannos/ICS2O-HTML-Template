487cd136d45ee145854470fcdace5ba3779f272f
evan-cagiannos <evan-cagiannos@users.noreply.github.com>
2022-04-20 17:26:52 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 // Created by Evan Cagiannos
 // ICS2O-UNIT2-02-HTML
-// Created on: 2022-March-10 
+// Created on: 2022-March-10
 //This file contains the JS functions for index.html
 
-'use strict' 
+"use strict";
 /**
  * This function calculates volume of pyramid.
  */
 function doMathClicked() {
   // this function does basic math
   // input
-  const radius = parseInt(document.getElementById('radius-of-sphere').value)
+  const radius = parseInt(document.getElementById("radius-of-sphere").value);
 
   // process
-  const volume = 4 / 3 (Math.PI * radius ** 3)
+  const volume = 4 / 3(Math.PI * radius ** 3);
 
   // output
-  document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm3'
+  document.getElementById("volume").innerHTML =
+    "Volume is: " + volume.toFixed(2) + " cm3";
 }
